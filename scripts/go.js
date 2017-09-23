268de0ccc69f876e3e59b1988053e7c2e8992cde
Spencer T. Parkin <spencertparkin@gmail.com>
2017-09-23 13:00:46 -0600

    make the back button work

--- scripts/go.js
+++ scripts/go.js
@@ -6,7 +6,7 @@ var OnNewGameClicked = function() {
         size = prompt( 'What size board?  (e.g. enter "5" for a 5x5 board.)' );
         $.getJSON( 'new_game', { 'name' : name, 'size' : size }, function( json_data ) {
             if( json_data.error )
-                window.location.replace( '/error_page?error=' + json_data.error );
+                window.location.assign( '/error_page?error=' + json_data.error );
             else
                 window.location.reload();
         } );
@@ -16,7 +16,7 @@ var OnNewGameClicked = function() {
 var OnDeleteGameClicked = function( name ) {
     $.getJSON( 'del_game', { 'name' : name }, function( json_data ) {
         if( json_data.error )
-            window.location.replace( '/error_page?error=' + json_data.error );
+            window.location.assign( '/error_page?error=' + json_data.error );
         else
             window.location.reload();
     } );
@@ -25,7 +25,7 @@ var OnDeleteGameClicked = function( name ) {
 var OnPlaceStoneClicked = function( name, color, row, col ) {
     $.getJSON( 'place_stone', { 'name' : name, 'color' : color, 'row' : row, 'col' : col }, function( json_data ) {
         if( json_data.error )
-            window.location.replace( '/error_page?error=' + json_data.error );
+            window.location.assign( '/error_page?error=' + json_data.error );
         else
             window.location.reload();
     } );
